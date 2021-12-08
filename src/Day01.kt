94ddf547455c2ed5711f9279a92f37b555e0fb28
John <john.a.glynn@gmail.com>
2021-12-07 21:26:32 -0600

    day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,20 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var deeperReadings = input.filterIndexed { i, v -> (i != 0) && (v > input.get(i-1))}
+        return deeperReadings.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var count = 0
+        for(i in 3 until input.size) {
+            val window1 = input[i-3] + input[i-2] + input[i-1]
+            val window2 = input[i-2] + input[i-1] + input[i]
+            if (window2 > window1) count++
+        }
+        return count
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = loadD1Input("Day01_test")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -7,6 +7,9 @@ import java.security.MessageDigest
  */
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
+fun loadD1Input(name: String): List<Int> {
+    return File("src", "$name.txt").readLines().map { it.toInt() }
+}
 /**
  * Converts string to md5 hash.
  */
